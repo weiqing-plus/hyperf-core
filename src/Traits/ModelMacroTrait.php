e49c91d60a7766eff45f87485063c5352489467e
卫青 <app.weiqing@gmail.com>
2024-02-02 01:46:49 +0800

    修正

--- src/Traits/ModelMacroTrait.php
+++ src/Traits/ModelMacroTrait.php
@@ -13,14 +13,26 @@ declare(strict_types=1);
 namespace Weiqing\HyperfCore\Traits;
 
 use Hyperf\Database\Model\Builder;
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\HttpServer\Contract\RequestInterface;
 
 trait ModelMacroTrait
 {
+
+    #[Inject]
+    private RequestInterface $request;
+
     private function registerBase()
     {
-        Builder::macro('getPageList', function ($perPage = 15) {
+        Builder::macro('getPageList', function ($perPage = null) {
+            // 从request中获取分页参数
+            $perPage ?: (int) $this->request->input('pageSize', 1);
             /* @var Builder $this */
             return $this->paginate($perPage);
+        });
+        Builder::macro('setSearch', function ($columns) {
+            /* @var Builder $this */
+            return $this;
         });
     }
 }
